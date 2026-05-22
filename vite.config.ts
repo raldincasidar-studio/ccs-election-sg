ba499f31f6047e7b864c3ade9f3fdc6e31b81da0
Raldin Casidar Studio <97776935+raldincasidar-studio@users.noreply.github.com>
2026-05-22 20:03:53 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,11 +18,11 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5000,
     allowedHosts: true,
-    proxy: {
+    /* proxy: {
       "/api": {
         target: "http://localhost:3001",
         changeOrigin: true,
       },
-    },
+    }, */
   },
 });
